5c01f6ad5e57fbbdbfaa8e079d0ea37e5b50700f
Mark Yates <93556548+markyates7748@users.noreply.github.com>
2022-03-11 11:47:57 -0700

    add missing ingress port rule for ALB

--- terraform/aline-infra-my/dev/modules/networking/main.tf
+++ terraform/aline-infra-my/dev/modules/networking/main.tf
@@ -34,6 +34,12 @@ resource "aws_security_group" "allow_ssh_http_tls"{
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    ingress {
+        from_port = 9443
+        to_port = 9443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     egress {
         from_port = 0
         to_port = 0
